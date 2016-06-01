5480fdf7688f5a5050eadaaf31cd330d6797b1d8
bnegrao <bnegrao@gmail.com>
2016-06-01 19:49:04 -0300

    improving the tests

--- test_shell_map.sh
+++ test_shell_map.sh
@@ -3,24 +3,25 @@
 source shell_map.sh
 
 die () {
-    echo "$0: ${1:-"Unknown Error"}" 1>&2
+    echo "$0: ${1:-"Unknown Error"} at line $2" 1>&2
     exit 1
 }
-
 
 # test instance creation
 shell_map new map1
 instance=$(compgen -A function "map1")
-[ -z "$instance" ] || [ "$instance" != "map1" ] && die "map1 instance wasn't created"
+[ -z "$instance" ] || [ "$instance" != "map1" ] && die "map1 instance wasn't created" $LINENO
 
 # test adding/retrieving KEY VALUE pairs
 map1 put John hired
-[ `map1 get John` == hired ] || die "Test failed"
+value="`map1 get John`"
+[ -z "$value" ] || [ "$value" != "not hired" ] || die "Test failed" $LINENO
 
 # key names with spaces aren't supported currently
 #map1 put "Camila Anderson" hired
 #[ `map1 get "Camila Anderson"` == hired ] || die "Test failed"
 
 # values with spaces aren't supported currently
-#map1 put Rodrigo "not hired"
-#[ `map1 get Rodrigo` == "not hired" ] || die "Test failed"
+map1 put Rodrigo "not hired"
+value="`map1 get Rodrigoo`"
+[ -z "$value" ] || [ "$value" != "not hired" ] && die "Test failed" $LINENO
