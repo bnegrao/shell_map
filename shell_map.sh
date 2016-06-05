bd79deb9d64d5ab334be2c7a950088b08cdbfadc
Bruno Negrão Guimarães Zica <bnegrao@gmail.com>
2016-06-05 16:37:33 -0300

    renaming croak to carp
    croak and carp are perl functions to report erros. But croak exits the program, while carp doesn't. So I renamed it to carp to make it more similar to Perl's function.

--- shell_map.sh
+++ shell_map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-croak () {
+carp () {
     echo "shell_map.sh: ERROR: $1" 1>&2
     return 1
 }
@@ -15,8 +15,8 @@ shell_map () {
     ;;
     put)
         local KEY="$2"
-        [ -z "$KEY" ] && return `croak "put() KEY cannot be empty."`
-        echo "$KEY" | grep -qPo '[^a-zA-Z0-9_]' && return `croak "put() KEY '$KEY' isn't valid. Valid KEY names can be letters, digits and underscores."`
+        [ -z "$KEY" ] && return `carp "put() KEY cannot be empty."`
+        echo "$KEY" | grep -qPo '[^a-zA-Z0-9_]' && return `carp "put() KEY '$KEY' isn't valid. Valid KEY names can be letters, digits and underscores."`
         local VALUE="$3"
         # declares a variable in the global scope
         eval ${FUNCNAME}_DATA_${KEY}='$VALUE'
@@ -34,7 +34,7 @@ shell_map () {
     ;;
     contains_key)
         local KEY="$2"
-        [ -z "$KEY" ] && return `croak "contains_key() KEY cannot be empty."`
+        [ -z "$KEY" ] && return `carp "contains_key() KEY cannot be empty."`
         compgen -v ${FUNCNAME}_DATA_${KEY} > /dev/null && return 0 || return 1
     ;;
     clear_all)
@@ -43,7 +43,7 @@ shell_map () {
     ;;
     remove)
         local KEY="$2"
-        [ -z "$KEY" ] && return `croak "remove() KEY cannot be empty."`
+        [ -z "$KEY" ] && return `carp "remove() KEY cannot be empty."`
         unset ${FUNCNAME}_DATA_${KEY}
     ;;
     size)
@@ -51,10 +51,10 @@ shell_map () {
     ;;
     put_increment)
         local KEY="$2"
-        [ -z "$KEY" ] && return `croak "put_increment() KEY cannot be empty."`
+        [ -z "$KEY" ] && return `carp "put_increment() KEY cannot be empty."`
         local NUMBER="$3"
-        [ -z "$NUMBER" ] && return `croak "put_increment() NUMBER cannot be empty."`
-        echo $NUMBER | grep -qPo '[^0-9]' && return `croak "pub_increment() NUMBER '$NUMBER' must be digits."`
+        [ -z "$NUMBER" ] && return `carp "put_increment() NUMBER cannot be empty."`
+        echo $NUMBER | grep -qPo '[^0-9]' && return `carp "pub_increment() NUMBER '$NUMBER' must be digits."`
 
         if `$FUNCNAME contains_key $KEY`; then
             local TOTAL=`$FUNCNAME get $KEY`
@@ -67,7 +67,7 @@ shell_map () {
         return 0
     ;;
     *)
-        return `croak "unsupported operation '$1'."`
+        return `carp "unsupported operation '$1'."`
     esac
 }
 
